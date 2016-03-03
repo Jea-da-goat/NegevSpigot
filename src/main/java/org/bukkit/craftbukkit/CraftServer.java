737551bfe22ebf7d840c395dd65ef89d62056486
kashike <kashike@vq.lc>
2016-03-03 02:15:57 -0600

    Expose server CommandMap

--- src/main/java/org/bukkit/craftbukkit/CraftServer.java
+++ src/main/java/org/bukkit/craftbukkit/CraftServer.java
@@ -1983,6 +1983,7 @@ public final class CraftServer implements Server {
         return this.helpMap;
     }
 
+    @Override // Paper - add override
     public SimpleCommandMap getCommandMap() {
         return this.commandMap;
     }
