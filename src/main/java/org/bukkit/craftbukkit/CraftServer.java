2713b0dcf48071f33cf1fa7b8ea2f26335cdb8fe
Aikar <aikar@aikar.co>
2016-03-18 13:17:38 -0400

    Default loading permissions.yml before plugins
    Under previous behavior, plugins were not able to check if a player had a permission
    if it was defined in permissions.yml. there is no clean way for a plugin to fix that either.
    
    This will change the order so that by default, permissions.yml loads BEFORE plugins instead of after.
    
    This gives plugins expected permission checks.
    
    It also helps improve the expected logic, as servers should set the initial defaults, and then let plugins
    modify that. Under the previous logic, plugins were unable (cleanly) override permissions.yml.
    
    A config option has been added for those who depend on the previous behavior, but I don't expect that.

--- src/main/java/org/bukkit/craftbukkit/CraftServer.java
+++ src/main/java/org/bukkit/craftbukkit/CraftServer.java
@@ -462,6 +462,7 @@ public final class CraftServer implements Server {
         if (type == PluginLoadOrder.STARTUP) {
             this.helpMap.clear();
             this.helpMap.initializeGeneralTopics();
+            if (io.papermc.paper.configuration.GlobalConfiguration.get().misc.loadPermissionsYmlBeforePlugins) loadCustomPermissions(); // Paper
         }
 
         Plugin[] plugins = this.pluginManager.getPlugins();
@@ -481,7 +482,7 @@ public final class CraftServer implements Server {
             this.commandMap.registerServerAliases();
             DefaultPermissions.registerCorePermissions();
             CraftDefaultPermissions.registerCorePermissions();
-            this.loadCustomPermissions();
+            if (!io.papermc.paper.configuration.GlobalConfiguration.get().misc.loadPermissionsYmlBeforePlugins) this.loadCustomPermissions(); // Paper
             this.helpMap.initializeCommands();
             this.syncCommands();
         }
