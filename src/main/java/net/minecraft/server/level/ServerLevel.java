15f0538c29e76b22a99ff4c2cf0a501462b223e9
Aikar <aikar@aikar.co>
2016-03-28 20:32:58 -0400

    Entity AddTo/RemoveFrom World Events

--- src/main/java/net/minecraft/server/level/ServerLevel.java
+++ src/main/java/net/minecraft/server/level/ServerLevel.java
@@ -2195,6 +2195,7 @@ public class ServerLevel extends Level implements WorldGenLevel {
                 entity.setOrigin(entity.getOriginVector().toLocation(getWorld()));
             }
             // Paper end
+            new com.destroystokyo.paper.event.entity.EntityAddToWorldEvent(entity.getBukkitEntity()).callEvent(); // Paper - fire while valid
         }
 
         public void onTrackingEnd(Entity entity) {
@@ -2270,6 +2271,7 @@ public class ServerLevel extends Level implements WorldGenLevel {
                 }
             }
             // CraftBukkit end
+            new com.destroystokyo.paper.event.entity.EntityRemoveFromWorldEvent(entity.getBukkitEntity()).callEvent(); // Paper - fire while valid
         }
 
         public void onSectionChange(Entity entity) {
