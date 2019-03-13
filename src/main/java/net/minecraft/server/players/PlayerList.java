661ae226a39d964d2b86df3d036c7a745b417fd6
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2019-03-13 20:08:09 +0200

    Call WhitelistToggleEvent when whitelist is toggled

--- src/main/java/net/minecraft/server/players/PlayerList.java
+++ src/main/java/net/minecraft/server/players/PlayerList.java
@@ -1129,6 +1129,7 @@ public abstract class PlayerList {
     }
 
     public void setUsingWhiteList(boolean whitelistEnabled) {
+        new com.destroystokyo.paper.event.server.WhitelistToggleEvent(whitelistEnabled).callEvent();
         this.doWhiteList = whitelistEnabled;
     }
 
