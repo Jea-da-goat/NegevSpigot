8720e7c3c97f79bc1aa3c8cece7cf871cf5a44a8
Aikar <aikar@aikar.co>
2020-06-28 19:36:55 -0400

    Don't allow null UUID's for chat

--- src/main/java/net/minecraft/network/chat/ChatSender.java
+++ src/main/java/net/minecraft/network/chat/ChatSender.java
@@ -8,6 +8,12 @@ import net.minecraft.world.entity.player.ProfilePublicKey;
 public record ChatSender(UUID profileId, @Nullable ProfilePublicKey profilePublicKey) {
     public static final ChatSender SYSTEM = new ChatSender(Util.NIL_UUID, (ProfilePublicKey)null);
 
+    // Paper start
+    public ChatSender {
+        com.google.common.base.Preconditions.checkNotNull(profileId, "uuid cannot be null");
+    }
+    // Paper end
+
     public boolean isSystem() {
         return SYSTEM.equals(this);
     }
