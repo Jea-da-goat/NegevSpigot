dc70938b33c0dd036074aef4dfe445d7d3e9c29c
José Miguel Moreno <josemmo@pm.me>
2021-06-05 13:45:15 +0200

    Fix plugin loggers on server shutdown

--- src/main/java/net/minecraft/server/MinecraftServer.java
+++ src/main/java/net/minecraft/server/MinecraftServer.java
@@ -1025,6 +1025,7 @@ public abstract class MinecraftServer extends ReentrantBlockableEventLoop<TickTa
             net.minecrell.terminalconsole.TerminalConsoleAppender.close(); // Paper - Use TerminalConsoleAppender
         } catch (Exception e) {
         }
+        io.papermc.paper.log.CustomLogManager.forceReset(); // Paper - Reset loggers after shutdown
         this.onServerExit();
         // Paper end
     }
