2fb21cd19c28f3401ed96f75972b36ab510ef595
Jake Potrebic <jake.m.potrebic@gmail.com>
2022-07-10 14:13:22 -0700

    Don't use level random in entity constructors
    Paper makes the entity random thread-safe
    and constructing an entity off the main thread
    should be supported. Some entities (for whatever
    reason) use the level's random in some places.

--- src/main/java/net/minecraft/world/entity/animal/Bee.java
+++ src/main/java/net/minecraft/world/entity/animal/Bee.java
@@ -1028,7 +1028,7 @@ public class Bee extends Animal implements NeutralMob, FlyingAnimal {
 
         BeeGoToHiveGoal() {
             super();
-            this.travellingTicks = Bee.this.level.random.nextInt(10);
+            this.travellingTicks = Bee.this./* level. */random.nextInt(10); // Paper - use entity random
             this.blacklistedTargets = Lists.newArrayList();
             this.setFlags(EnumSet.of(Goal.Flag.MOVE));
         }
@@ -1145,7 +1145,7 @@ public class Bee extends Animal implements NeutralMob, FlyingAnimal {
 
         BeeGoToKnownFlowerGoal() {
             super();
-            this.travellingTicks = Bee.this.level.random.nextInt(10);
+            this.travellingTicks = Bee.this./* level. */random.nextInt(10); // Paper - use entity random
             this.setFlags(EnumSet.of(Goal.Flag.MOVE));
         }
 
