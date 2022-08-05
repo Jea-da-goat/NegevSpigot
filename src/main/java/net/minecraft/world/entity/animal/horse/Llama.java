1f6c63976488d7c088a26d49be3b3fdb2bb34828
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2022-08-05 12:16:51 +0200

    Missing eating regain reason

--- src/main/java/net/minecraft/world/entity/animal/horse/Llama.java
+++ src/main/java/net/minecraft/world/entity/animal/horse/Llama.java
@@ -211,7 +211,7 @@ public class Llama extends AbstractChestedHorse implements RangedAttackMob {
         }
 
         if (this.getHealth() < this.getMaxHealth() && f > 0.0F) {
-            this.heal(f);
+            this.heal(f, org.bukkit.event.entity.EntityRegainHealthEvent.RegainReason.EATING); // Paper
             flag = true;
         }
 
