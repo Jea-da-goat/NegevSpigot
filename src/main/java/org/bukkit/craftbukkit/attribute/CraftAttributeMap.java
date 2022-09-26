PaperMC/Paper

living entity allow attribute registration

paper-server/src/main/java/org/bukkit/craftbukkit/attribute/CraftAttributeMap.java
@@ -35,4 +35,11 @@ public class CraftAttributeMap implements Attributable {
 
         return (nms == null) ? null : new CraftAttributeInstance(nms, attribute);
     }
+    // Paper start - living entity allow attribute registration
+    @Override
+    public void registerAttribute(Attribute attribute) {
+        Preconditions.checkArgument(attribute != null, "attribute");
+        handle.registerAttribute(CraftAttribute.bukkitToMinecraftHolder(attribute));
+    }
+    // Paper end - living entity allow attribute registration
 }

Jea-da-goat/NegevSpigot

src/main/java/org/bukkit/craftbukkit/attribute/CraftAttributeMap.java
@@ -38,6 +38,14 @@ public class CraftAttributeMap implements Attributable {
         return (nms == null) ? null : new CraftAttributeInstance(nms, attribute);
     }
 
+    // Paper start
+    @Override
+    public void registerAttribute(Attribute attribute) {
+        Preconditions.checkArgument(attribute != null, "attribute");
+        handle.registerAttribute(CraftAttributeMap.toMinecraft(attribute));
+    }
+    // Paper end
+
     public static net.minecraft.world.entity.ai.attributes.Attribute toMinecraft(Attribute attribute) {
         return net.minecraft.core.Registry.ATTRIBUTE.get(CraftNamespacedKey.toMinecraft(attribute.getKey()));
     }